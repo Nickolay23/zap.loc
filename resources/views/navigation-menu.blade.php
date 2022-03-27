0f27698a4db82028a4d1122cdfefa1180b6f0090
Nickolay2312 <61413110+Nickolay2312@users.noreply.github.com>
2022-03-27 23:54:34 +0300

    Добавил роли и политики

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,11 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    @if (auth()->user()->is_admin)
+                        <x-jet-nav-link href="{{ route('admin.products.index')}}" :active="request()->routeIs('admin.products.index')">
+                            Товары
+                        </x-jet-nav-link>
+                    @endif
                 </div>
             </div>
 
